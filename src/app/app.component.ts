f4d8ec980c5995450ac606b94864a83791b97f0d
Romario Rodriguez <romariorodriguez17@gmail.com>
2024-10-31 16:19:36 -0500

    fix(develop) se solucionan errores con la libreria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,22 @@ import { PrimeNGConfig } from 'primeng/api';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent implements OnInit {
+
+  loading: boolean = false;
 
   constructor(private primengConfig: PrimeNGConfig) { }
 
   ngOnInit() {
     this.primengConfig.ripple = true;
   }
+
+  load() {
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+    }, 5000
+    )
+  };
 }
