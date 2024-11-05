aceb6452a462e93bfa96b0ddd05a6f1d48f442fa
Romario Rodriguez <romariorodriguez17@gmail.com>
2024-11-05 00:55:46 -0500

    feat(datepipe) Se añade de formal global el idioma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+//configuracion del local de la app
+import localeEsCO from '@angular/common/locales/es-CO'
+import localeFrCA from '@angular/common/locales/fr-CA'
+import { registerLocaleData } from '@angular/common';
+
+
+registerLocaleData(localeEsCO);
+registerLocaleData(localeFrCA);
 
 
 @NgModule({
@@ -18,7 +27,8 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    { provide: LOCALE_ID, useValue: 'es-CO' }
   ],
   bootstrap: [AppComponent]
 })
